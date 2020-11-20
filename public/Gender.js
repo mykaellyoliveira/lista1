49a5ed4a9e96dcd26c2fc0320833f0de91b21012
mykaellyoliveira <mykaelly.odc02@gmail.com>
2020-11-20 16:38:37 -0300

    Alteração 300

--- public/Gender.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Gender;
-(function (Gender) {
-    Gender["Female"] = "f";
-    Gender["Male"] = "m";
-})(Gender || (Gender = {}));
-exports.default = Gender;
